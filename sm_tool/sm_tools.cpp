2c78865f1ff1a3c2c8f2fa22a0d8489d2e1562aa
leipeng <258441601@qq.com>
2016-08-29 18:16:05 +0800

    添加项目文件。

---
+++ sm_tool/sm_tools.cpp
@@ -0,0 +1,168 @@
+#include"sm_hash_map.h"
+#include<string>
+#include<iostream>
+#include<vector>
+#include<random>
+#include<boost/progress.hpp>
+#include<boost/algorithm/string.hpp>
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <boost/interprocess/sync/named_mutex.hpp>
+using namespace std;
+using namespace boost::algorithm;
+using namespace boost::interprocess;
+
+static SM_HANDLE handle;
+static void info()
+{
+	cout << "Tool for shared memory hashmap\n"
+		"Copyright SZKINGDOM Inc. All Rights Reserved\n"
+		"Author: leipeng@szkingdom.com\n\n";
+}
+static void help()
+{
+	cout << "usage:\n"
+		"    use [namespace]\n"
+		"    show: list all namespace\n"
+		"    get key\n"
+		"    set key value\n"
+		"    list [number]\n"
+		"    info\n"
+		"    quit\n" 
+		"    help\n"<< endl;;
+}
+static string trimm(string s)
+{
+	trim(s);
+	return s;
+}
+int maint()
+{
+	info();
+	help();
+	string input;
+	while (std::getline(cin, input))
+	{
+		if (trimm(input) == "show")
+		{
+			try {
+				shared_memory_object smo(open_only, "sm_namespaces", read_only);
+				mapped_region rg(smo, read_only);
+				char *q = static_cast<char*>(rg.get_address());
+				while (*q)
+				{
+					cout << q << endl;
+					q += 128;
+				}
+			}
+			catch (const exception&)
+			{
+				cout << "no namespace exsits" << endl;
+			}
+
+			continue;
+		}
+
+		if (starts_with(input, "use"))
+		{
+			if (handle)
+				sm_release(handle);
+			handle = sm_client_init(trimm(input.substr(3)).c_str());
+
+			if (!handle)
+			{
+				cout << "failed: namespace " << trimm(input.substr(3)) << " not exist or tool version not match" << endl;
+			}
+			continue;
+		}
+
+		if (input == "h" || input == "help")
+		{
+			help();
+		}
+
+		else if (input == "q" || input == "quit")
+		{
+			break;
+		}
+
+		else if (!handle)
+		{
+			cout << "use namespace first!" << endl;
+			continue;
+		}
+
+		else if (input == "info")
+		{
+			cout <<"version "<<sm_version()
+				<< "\nsize " << sm_size(handle)
+				<< "\naverage depth " << sm_avg_depth(handle)
+				<< "\nmemory used " << sm_memory_use(handle)
+				<< "\nmemory allocated " << sm_total_memory(handle)
+				<< "\nmax key length " << sm_key_len(handle)
+				<< "\nmax value length " << sm_value_len(handle) << "\n" << endl;
+		}
+
+		else if (starts_with(input, "list"))
+		{
+			size_t n;
+			if (trimm(input.substr(4)).empty())
+				n = 20;
+			else
+				n = stoi(trimm(input.substr(4)));
+
+			if (n > sm_size(handle))
+				n = sm_size(handle);
+
+			auto kvi_size = sm_key_len(handle) + sm_value_len(handle) + sizeof size_t;
+			std::random_device rd;
+			std::default_random_engine engine(rd());
+			std::uniform_int_distribution<> distribution(0, sm_size(handle) - n);
+			auto p = sm_bucket_head(handle) + distribution(engine)*kvi_size;
+			printf("%-32s%s\n", "key", "value");
+			printf("---------------------------------------------\n");
+			for (size_t i = 0; i < n;)
+			{
+				if (*p)
+				{
+					printf("%-32s%s\n", p, p + sm_key_len(handle));
+					++i;
+				}
+				p += kvi_size;
+			}
+			cout << endl;
+		}
+
+		else if (starts_with(input, "get"))
+		{
+			string key = trimm(input.substr(3));
+			char value[4096];
+			size_t len = 4096;
+			int error = sm_get(handle, key.c_str(), value, len);
+			if (error != 0)
+				cout << "sm_get error: " << error << endl;
+			else
+				cout << value << endl;
+		}
+
+		else if (starts_with(input, "set"))
+		{
+			string kv = trimm(input.substr(3));
+			auto finder = kv.find_first_of(" ");
+			if (finder == string::npos)
+			{
+				cout << "failed: set formmat error" << endl;
+				continue;
+			}
+			string key = kv.substr(0, finder);
+			string value = trimm(kv.substr(finder));
+			int error = sm_set(handle, key.c_str(), value.c_str());
+			if (error != 0)
+				cout << "sm_set error: " << error << endl;
+		}
+
+		else
+			cout << "command not found" << endl;
+	}
+	return 0;
+}
---
+++ sm_tool/源1.cpp
@@ -0,0 +1,55 @@
+#include<all.h>
+#include <boost/progress.hpp>
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <boost/interprocess/sync/named_mutex.hpp>
+#include<boost/algorithm/string.hpp>
+#include<mutex>
+#include<random>
+using namespace boost::interprocess;
+struct ba
+{
+	ba() {}
+	void start() { go(); }
+	virtual void go() { cout << "ba go" << endl; }
+	~ba() { cout << "~ba" << endl; }
+};
+struct sa :ba
+{
+
+	void go() { cout << "sa go" << endl; };
+	~sa() { cout << "~sa" << endl; }
+};
+
+long long counts = 0;
+const int n = 1000000000;
+static named_mutex mtxb{ open_or_create, "cc5" };
+static mutex mm;
+
+
+void gg()
+{
+	const int sz = 1000 * 1000 * 100;
+
+	shared_memory_object shm(open_or_create, "cs", read_write);
+	shm.truncate(sz);
+	char *p;
+	{
+		std::shared_ptr<mapped_region> region;
+		region = make_shared<mapped_region>(shm, read_write);
+		p = (static_cast<char*>(region->get_address()));
+		cout << (int)p << endl;
+	}
+	shm.truncate(sz*2);
+	{
+		std::shared_ptr<mapped_region> region;
+		region = make_shared<mapped_region>(shm, read_write);
+		p = (static_cast<char*>(region->get_address()));
+		cout << (int)p << endl;
+	}
+}
+
+int main()
+{
+	gg();
+}
